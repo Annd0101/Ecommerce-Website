f40d08b395b2b00d09ccc984901e1a4e5bf5c431
An_Edric <ducan.co.6789@gmai.com>
2023-09-29 22:09:11 +0700

    category api

--- src/main/java/com/Ecommerce/Jewelry/controller/CategoryController.java
+++ src/main/java/com/Ecommerce/Jewelry/controller/CategoryController.java
@@ -1,13 +1,16 @@
 package com.Ecommerce.Jewelry.controller;
 
 
+import com.Ecommerce.Jewelry.common.ApiResponse;
 import com.Ecommerce.Jewelry.model.Category;
 import com.Ecommerce.Jewelry.service.CategoryService;
+import io.swagger.annotations.Api;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/category")
@@ -16,8 +19,20 @@ public class CategoryController {
     CategoryService categoryService;
 
     @PostMapping("/create")
-    public String createCategory(@RequestBody Category category){
+    public ResponseEntity<ApiResponse> createCategory(@RequestBody Category category){
         categoryService.createCategory(category);
-        return "success";
+        return new ResponseEntity<>(new ApiResponse(true,"A new category is created"), HttpStatus.CREATED);
+    }
+    @GetMapping("/list")
+    public List<Category> listCategory() {
+        return categoryService.listCategory();
+    }
+    @PostMapping("/category/{categoryId}")
+    public ResponseEntity<ApiResponse> updateCategory(@PathVariable int categoryId, @RequestBody Category category){
+        if(!categoryService.findById(categoryId)){
+            return new ResponseEntity<>(new ApiResponse(false,"Category does not exist!"), HttpStatus.NOT_FOUND);
+        }
+        categoryService.updateCategory(categoryId, category);
+        return new ResponseEntity<>(new ApiResponse(true,"Category updated"),HttpStatus.OK);
     }
 }
